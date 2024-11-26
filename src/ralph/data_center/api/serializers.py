68030f34c17d70c3c6b53a001954fa57785aee4f
Paweł Szulc <pawel.szulc@allegro.com>
2024-11-26 11:18:15 +0100

    Fix related hosts

--- src/ralph/data_center/api/serializers.py
+++ src/ralph/data_center/api/serializers.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from django.contrib.contenttypes.models import ContentType
 from rest_framework import serializers
 
 from ralph.api import RalphAPISerializer
@@ -27,7 +26,6 @@ from ralph.data_center.models import (
     VIP
 )
 from ralph.security.api import SecurityScanSerializer
-from ralph.virtual.models import VirtualServer
 
 
 class ClusterTypeSerializer(RalphAPISerializer):
@@ -132,40 +130,32 @@ class DataCenterAssetSerializer(ComponentSerializerMixin, AssetSerializer):
     securityscan = SecurityScanSerializer()
     related_hosts = serializers.SerializerMethodField()
 
-    def _get_serialized_sublist(self, full_list, serializer_class, cond):
+    def _get_serialized_sublist(self, full_list, serializer_class):
         return serializer_class(
-            [elem for elem in full_list if cond(elem)],
+            full_list,
             many=True, context=self.context
         ).data
 
     def _get_physical_servers(self, obj):
-        dca = ContentType.objects.get_for_model(DataCenterAsset)
         return self._get_serialized_sublist(
-            obj.children.all(),
-            DataCenterAssetSimpleSerializer,
-            lambda child: child.content_type == dca
+            obj.physical_servers,
+            DataCenterAssetSimpleSerializer
         )
 
     def _get_virtual_servers(self, obj):
         from ralph.virtual.api import VirtualServerSimpleSerializer
-        vs = ContentType.objects.get_for_model(VirtualServer)
         return self._get_serialized_sublist(
-            obj.children.all(),
-            VirtualServerSimpleSerializer,
-            lambda child: child.content_type == vs
+            obj.virtual_servers,
+            VirtualServerSimpleSerializer
         )
 
     def _get_cloud_hosts(self, obj):
         from ralph.virtual.api import CloudHostSimpleSerializer
         return self._get_serialized_sublist(
-            obj.cloudhost_set.all(),
-            CloudHostSimpleSerializer,
-            lambda host: True
-        )
+            obj.cloud_hosts,
+            CloudHostSimpleSerializer)
 
     def get_related_hosts(self, obj):
-        if hasattr(obj, 'content_type'):
-            obj = obj.content_type.get_object_for_this_type(pk=obj.pk)
         return {
             "virtual_servers": self._get_virtual_servers(obj),
             "physical_servers": self._get_physical_servers(obj),
